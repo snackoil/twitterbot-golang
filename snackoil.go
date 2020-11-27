b296168c0538ee25f2fc495de5d7d8e732462801
snackoil <74830816+snackoil@users.noreply.github.com>
2020-11-27 23:19:57 +0100

    Create snackoil.go

---
+++ snackoil.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+var (
+	ConsumerKey       = os.Getenv("CONSUMER_KEY")
+	ConsumerKeySecret = os.Getenv("CONSUMER_SECRET")
+	AccessToken       = os.Getenv("ACCESS_TOKEN")
+	AccessSecret      = os.Getenv("ACCESS_TOKEN_SECRET")
+)
+
+func retweet(){
+	config := oauth1.NewConfig(ConsumerKey, ConsumerKeySecret)
+	token := oauth1.NewToken(AccessToken, AccessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Client
+	client := twitter.NewClient(httpClient)
+	verifyParams := &twitter.AccountVerifyParams{
+		IncludeEmail: twitter.Bool(true),
+	}
+	user, _, _ := client.Accounts.VerifyCredentials(verifyParams)
+	fmt.Printf("Username:%+v\n", user.ScreenName)
+
+	// Search the last 20 #snackoil to retweet
+	searchParams := &twitter.SearchTweetParams{
+		Query: "%23" + "#snackoil",
+		Count: 20,
+	}
+	searchResult, _, _ := client.Search.Tweets(searchParams)
+	if len(searchResult.Statuses) == 0 {
+		os.Exit(0)
+	}
+	for _, tweet := range searchResult.Statuses {
+		client.Statuses.Retweet(tweet.ID, &twitter.StatusRetweetParams{})
+		fmt.Printf("RETWEETED: %+v\n", tweet.Text)
+	}
+}
+
+func main() {
+	for {
+		go retweet()
+		time.Sleep(10 * time.Second)
+	}
+}
